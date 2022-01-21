005ba94f39ecfd958737daee287ef667cde41416
RicardoCabreraMarquez <rmcabmar@adaits.es>
2022-01-21 19:00:11 +0100

    Create Example.java

---
+++ Entorno Desarrollo/Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        a = a + 1;
+        c = c + a + b;
+        b = 7;
+        a = 8;
+        c = 6 + b + c;
+    }
+}
